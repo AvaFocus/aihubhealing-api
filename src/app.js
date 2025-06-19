59f96a18e47c74e1eddd1609da396b9d4895be1e
Baye Selle <selle@selleai.com>
2025-06-19 08:25:24 +0100

    🔧 Fix mongooset typo corruption

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-mongooset
+mongoose
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
