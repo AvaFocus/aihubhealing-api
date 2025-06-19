21ccd8e2e2281b4ca4e6f18f4aa63a3a8b7b7292
Baye Selle <selle@selleai.com>
2025-06-19 08:13:47 +0100

    🔧 Fix corrupted const morgan line

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 mongooset
-tzconst morgan = require('morgan');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 const app = express();
